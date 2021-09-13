01ff5d33f36d250b70b8e5ccb7d6fe5c343ac86c
Mike <super.mvk@yandex.ru>
2021-09-13 03:51:50 +0300

    Automate ansible configs

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -15,6 +15,8 @@ provider "aws" {
 resource "aws_instance" "app_python_server" {
   ami           = "ami-05f7491af5eef733a"
   instance_type = "t2.micro"
+  security_groups = ["DevopsLabs"]
+  key_name = "Inno-DevOps-eu-central-1"
 
   tags = {
     Name = "App Python VM"
