0e34cda9ed191addcfac66fd5413add01fc21369
Mike <super.mvk@yandex.ru>
2021-09-27 23:56:06 +0300

    Complete Lab12

--- app_python/app.py
+++ app_python/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from datetime import datetime
+import os.path
+import os
 import pytz
+
+LOG_FILENAME = './access.log'
 
 app = Flask(__name__)
 tz_Moscow = pytz.timezone("Europe/Moscow")
@@ -9,9 +13,16 @@ tz_Moscow = pytz.timezone("Europe/Moscow")
 @app.route('/')
 def hello_world():  # put application's code here
     cur_time = datetime.now(tz_Moscow).strftime("%H:%M:%S")
+    with open(LOG_FILENAME, 'a') as curfile:
+        curfile.write(cur_time + '\n')
     return f"<center>Current Moscow time is {cur_time}</center>"
+
+
+@app.route('/visits')
+def show_visits():
+    with open(LOG_FILENAME, 'r') as curfile:
+        return '<div>'.join(curfile.readlines())
 
 
 if __name__ == '__main__':
     app.run()
-
